ceac94b1092940327727771e8c7f25e5f4cba1bc
Sergey Tevs <tevs.sergey@gmail.com>
2024-04-10 15:39:22 +0200

    Update Core.php

---
+++ src/Core.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Installer;
+
+use Composer\Installer\LibraryInstaller;
+use Composer\Package\PackageInterface;
+use Composer\Repository\InstalledRepositoryInterface;
+use React\Promise\PromiseInterface;
+
+class Core extends LibraryInstaller {
+
+    public function supports($packageType) {
+        return $packageType === 'core';
+    }
+
+    public function getInstallPath(PackageInterface $package) {
+        return "core/";
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function download(PackageInterface $package, PackageInterface $prevPackage = null)
+    {
+        // TODO: Implement download() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function prepare($type, PackageInterface $package, PackageInterface $prevPackage = null)
+    {
+        // TODO: Implement prepare() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function cleanup($type, PackageInterface $package, PackageInterface $prevPackage = null)
+    {
+        // TODO: Implement cleanup() method.
+    }
+}
